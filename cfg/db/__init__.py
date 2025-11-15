ea29268ef9b7199ae9ab58daa5949bd388e999e9
MathieuAudibert <mathieu.audibert27@gmail.com>
2025-11-15 23:58:27 +0100

    feat(init):

--- cfg/db/__init__.py
+++ cfg/db/__init__.py
@@ -1,1 +1,8 @@
 __version__ = "2.0"
+__description__ = "Package to handle database"
+
+from .classes.connect.connectAdmin import ConnectDbAdm
+# from .classes.connect.connect import ConnectDb
+from db import ConnectDbAdm
+
+__all__ = ['ConnectDbAdm']
