95348307e1215db7fb9458c0fa27b974d71a946b
MathieuAudibert <mathieu.audibert27@gmail.com>
2025-11-16 00:08:15 +0100

    fix(typo): cleaned

--- backend/src/etl/extract.py
+++ backend/src/etl/extract.py
@@ -17,26 +17,26 @@ def _resolve_path(file_path):
     project_root = Path(__file__).resolve().parents[2]
 
     fp = Path(file_path)
-    if fp.parts and fp.parts[0] == "data":
+    if fp.parts and fp.parts[0] == "cfg":
         return (project_root / fp).resolve()
     else:
-        return (project_root / "data" / fp).resolve()
+        return (project_root / "cfg" / fp).resolve()
 
 
 def extract(file_path):
-    logger.info(f"Début de l'extraction des données depuis {file_path}")
+    logger.info(f"[INFO]: Début de l'extraction des données depuis {file_path}")
     try:
         path = _resolve_path(file_path)
-        logger.debug(f"Chemin résolu pour l'extraction : {path}")
+        logger.debug(f"[INFO]: Chemin résolu pour l'extraction : {path}")
         cars = pd.read_csv(path)
-        logger.info(f"Extraction réussie : {len(cars)} lignes extraites.")
+        logger.info(f"[INFO]: Extraction réussie : {len(cars)} lignes extraites.")
         return cars
     except Exception as e:
-        logger.error(f"Erreur lors de l'extraction des données depuis {file_path} : {e}")
+        logger.error(f"[ERROR]: Erreur lors de l'extraction des données depuis {file_path} : {e}")
         raise
 
 
 if __name__ == "__main__":
-    default_file = os.environ.get("DEFAULT_FILE", "raw/pre-owned cars.csv")
+    default_file = os.environ.get("DEFAULT_FILE", "raw/pre-owned dataset-cars-dirty.csv")
     cars = extract(default_file)
     print(cars.head())
