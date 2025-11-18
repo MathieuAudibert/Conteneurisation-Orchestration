48e725bf53bd0f7ebfd6e81ea3da437fe6412ad1
MathieuAudibert <mathieu.audibert27@gmail.com>
2025-11-18 23:51:45 +0100

    feat(version): and new module Doctrine

--- cfg/db/__init__.py
+++ cfg/db/__init__.py
@@ -1,7 +1,8 @@
-__version__ = "2.2"
+__version__ = "3.2"
 __description__ = "Module qui gere la bdd"
 
 from .classes.connect import ConnectDB
+from .classes.doctrine.select import DoctrineSelect
 from db import ConnectDB
 
-__all__ = ['ConnectDB']
+__all__ = ['ConnectDB', 'DoctrineSelect']
