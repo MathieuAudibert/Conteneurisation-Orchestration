fda02f31e86059d2ee5e6ea5a62b70729b1c23da
MathieuAudibert <mathieu.audibert27@gmail.com>
2025-11-18 23:02:25 +0100

    feat(dotenv): proper .env path

--- cfg/db/backup/backup.sh
+++ cfg/db/backup/backup.sh
@@ -8,7 +8,7 @@ echo "[$TODAY]: starting mongodb backup"
 extractMongo() {
     # recupere les variables d'env du .env
     set -a
-    . .env
+    . ../../../.env
     set +a
 
     mongosh "$ADM_MONGO_URI" --eval "const cars=db.cars.find().toArray(); const logs=db.logs.find().toArray(); print(JSON.stringify({cars, logs}, null, 2));" > "files/${TODAY}.json"
