1e34709c43c212776907c2e004cf13a8dbcdec1d
MathieuAudibert <mathieu.audibert27@gmail.com>
2025-12-26 14:42:03 +0100

    fix(filetree): useless

--- cfg/db/__init__.py
+++
@@ -1,7 +0,0 @@
-__version__ = "3.2"
-__description__ = "Module qui gere la bdd"
-
-from .classes.connect import ConnectDB
-from .classes.doctrine import Doctrine
-
-__all__ = ['ConnectDB', 'Doctrine']
--- cfg/db/test/testdelete.py
+++
@@ -1,5 +0,0 @@
-from cfg.db.classes.doctrine import Doctrine
-
-d = Doctrine()
-spr = d.delete('cars', {'brand': 'peugeot'})
-print(f"[INFO]: deleted - {spr}")
