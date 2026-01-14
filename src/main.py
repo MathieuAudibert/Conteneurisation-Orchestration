54bd3545e2a42b29d7bbbad783e5a4d1c55745ab
MathieuAudibert <mathieu.audibert27@gmail.com>
2026-01-14 17:45:57 +0100

    feat: fixed docker compose and docs

--- src/main.py
+++ src/main.py
@@ -1,11 +1,19 @@
 from fastapi import FastAPI
+from src.api.v1.dataset.get_dataset import router as dataset_route
+from fastapi.staticfiles import StaticFiles
 
-app = FastAPI(title="Conteneurisation-Orchestration", description="Etl")
+app = FastAPI(title="Conteneurisation-Orchestration", description="Etl", docs_url="/api/documentation")
+app.include_router(dataset_route)
+app.mount("/docs", StaticFiles(directory=".github/site", html=True), name="docs")
 
 @app.get("/")
 def read_root() -> dict:
     return {
         "statusCode": 200,
         "app": "Conteneurisation-Orchestration", 
-        "endpoints": [""]
+        "endpoints": {
+            "/docs": "Documentation of the project",
+            "/api/documentation": "Swagger-ui documentation",
+            "/dataset": "get the dataset"
+        }
     }
