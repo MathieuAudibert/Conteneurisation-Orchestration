a810c0b2e3b2a861e6d30bafd6b2c6f1fdbe59fa
MathieuAudibert <mathieu.audibert27@gmail.com>
2026-01-14 18:20:15 +0100

    feat: load endpoint

--- src/api/v1/etl/load.py
+++ src/api/v1/etl/load.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
+from src.etl.extract import extract
 
 router = APIRouter(prefix="/etl", tags=["etl"])
 
 @router.get("/load")
 def run_load():
-    pass
+    data = extract("cfg/dataset-cars-dirty.csv")
+    return {"status": "success", "message": "Data extracted successfully", "data": data}
--- src/main.py
+++ src/main.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI
-from src.api.v1.dataset.get_dataset import router as dataset_route
 from fastapi.staticfiles import StaticFiles
+from src.api.v1.dataset.get_dataset import router as dataset_route
+from src.api.v1.etl.load import router as load_route
 
 app = FastAPI(title="Conteneurisation-Orchestration", description="Etl", docs_url="/api/documentation")
-app.include_router(dataset_route)
 app.mount("/docs", StaticFiles(directory=".github/site", html=True), name="docs")
+app.include_router(dataset_route)
+app.include_router(load_route)
 
 @app.get("/")
 def read_root() -> dict:
@@ -12,6 +14,9 @@ def read_root() -> dict:
         "statusCode": 200,
         "app": "Conteneurisation-Orchestration", 
         "endpoints": {
+            "/etl": {
+                "/etl/load": "load the dataset"
+            },
             "/docs": "Documentation of the project",
             "/api/documentation": "Swagger-ui documentation",
             "/dataset": "get the dataset"
