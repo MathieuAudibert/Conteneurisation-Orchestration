c79631c24242a3e9d6becfb0d1931b2daef0aac8
BIGAND Theo <theobigand@gmail.com>
2026-02-11 15:51:05 +0100

    fix: add return type hint for get_router function

--- backend/src/api/v1/dataset/get_dataset.py
+++ backend/src/api/v1/dataset/get_dataset.py
@@ -1,9 +1,10 @@
+from typing import Any, Hashable
 from fastapi import APIRouter
 import pandas as pd
 
 router = APIRouter(prefix="/api/v1/dataset", tags=["dataset"])
 
 @router.get("/")
-def get_router():
+def get_router() -> list[dict[Hashable, Any]]:
     df = pd.read_csv(filepath_or_buffer="cfg/dataset-cars-dirty.csv")
     return df.fillna("").to_dict(orient="records")
